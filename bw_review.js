ef7873ad854910cfb03cc9c1bce78cfa5999b79b
Melinda Chirila <mchirila980@west-mec.org>
2019-03-26 15:10:52 -0700

    Final commit
    project is ready for review.

--- bw_review.js
+++ bw_review.js
@@ -40,36 +40,55 @@ window.onload = init();
 //
 
 function init() {
-      var stars = document.querySelectionAll("span#stars img");
+      var stars = document.querySelectorAll("span#stars img");
       for (var i = 0; i < stars.length; i++) {
             stars[i].style.cursor = "pointer";
             stars[i].addEventListener('mouseenter', lightStars);
       }
-      document.getElementById("comment").addEventListener("keyup", updateCOunt);
+      document.getElementById("comment").addEventListener("keyup", updateCount);
 }
 
-//
+//stars function for the rating (5 stars total)
 
 function lightStars() {
-      var startNumber = event.target.alt;
-      var stars = document.querySelectorAll("span#stars img")
-      for (var i = 0; i < startNumber; i++) {
-            stars[i].scr = "bw_star2.png";
+      var starNumber = event.target.alt;
+      var stars = document.querySelectorAll("span#stars img");
+      for (var i = 0; i < starNumber; i++) {
+            stars[i].src = "bw_star2.png";
       };
 
-      for (var i = startNumber; i < 5; i++) {
+      for (var i = starNumber; i < 5; i++) {
             stars[i].scr = "bw_star.png";
       };
 
-      ducument.getElementById("rating").value = startNumber + "stars";
-      event.target.addEventListener("mouseLeave", turnOffStars);
+      document.getElementById("rating").value = starNumber + "stars";
+      event.target.addEventListener("mouseleave", turnOffStars);
       event.target.addEventListener("click", function () {
-            event.target.removeEventListener("mouseLeave", turnOffStars);
+            event.target.removeEventListener("mouseleave", turnOffStars);
       })
 }
 
+//taking the stars off
 function turnOffStars() {
-      var stars = document.querySelectorAll("span#stars img") 
+      var stars = document.querySelectorAll("span#stars img")
+      for(var i = 0; i < stars.length; i++) {
+            stars[i].src = "bw_star.png";
+      }
+      document.getElementById("rating").value = "";
+}
+
+//if the character count limit goes over 1000 then it will turn red.
+      var commentText = document.getElementById("comment").value;
+      var charCount = countCharacters(commentText);
+      var wordCountBox = document.getElementById("wordCount");
+      wordCountBox.value = charCount = "/1000";
+if(charCount > 1000) {
+      wordCountBox.style.color = "white";
+      wordCountBox.style.backgroundColor = "red";
+}
+else {
+      wordCountBox.style.colro = "black";
+      wordCountBox.style.backgroundColor = "write";
 }
 
 
